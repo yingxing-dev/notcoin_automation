7937b87fdf0a2218fec8d18cfea437906dfef181
Yingxing <118210930+yingxing-dev@users.noreply.github.com>
2024-01-09 02:26:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
-globalscore = 1000
+powerLimitForAutotap = 100
 countclicks = 34
+recharging = false
+skipClick = false
+
 async function click() {
     let cc = document.querySelectorAll('div[class^="_notcoin"]');
     let scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
     let score = parseInt(scoreElement.textContent);
-
+    
     try {
         let imrocket = document.querySelectorAll('img[class^="_root"]');
         imrocket[0][Object.keys(imrocket[0])[1]].onClick();
     } catch (error) {}
-
+    
     for (let step = 0; step < countclicks; step++) {
         score = parseInt(scoreElement.textContent);
 
-        if (score > globalscore) {
+        if (skipClick) {
+            break;
+        }
+        
+        if (recharging) {
+            if (score >= powerLimitForAutotap) {
+                recharging = false;
+            }
+            break;
+        }
+
+        if (score > 0) {
             try {
                 await new Promise((resolve) => {
                     cc[0][Object.keys(cc[0])[1]].onTouchStart('');
@@ -21,9 +35,18 @@ async function click() {
                 });
             } catch (error) {}
         } else {
+            recharging = true;
             break;
         }
     }
 }
 
 setInterval(click, 500);
+
+function start() {
+    skipClick = false;
+}
+
+function stop() {
+    skipClick = true;
+}
